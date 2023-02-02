41000ba68e2d86e0ce0c87739916979bab183db7
p0ptartz <ericwm420@gmail.com>
2023-02-01 22:43:55 -0500

    required classes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const inquirer = require("inquirer")
+const Engineer = require("./lib/Engineer")
+const Manager = require("./lib/Manager")
+const Intern = require("./lib/Intern")
+const fs = require("fs")
+
+
