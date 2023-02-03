19103d407f3d8931d671845759cb864fef79d8dc
p0ptartz <ericwm420@gmail.com>
2023-02-02 19:33:15 -0500

    basic prompt function to run

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const inquirer = require("inquirer")
+const Employee = require("./lib/Employee")
 const Engineer = require("./lib/Engineer")
 const Manager = require("./lib/Manager")
 const Intern = require("./lib/Intern")
 const fs = require("fs")
 
 
+
+
+
+const managerQuestions = [{
+    type: "input",
+    name: "managerName",
+    message: "What is your name?",
+}
+]
+
+init = () => {
+    inquirer.prompt(managerQuestions)
+}
+
+init()
